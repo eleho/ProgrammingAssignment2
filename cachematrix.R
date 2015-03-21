9d2d19c1b21191f4af81ddbaf4be9d0a15dc34af
eleho <ed.eleho@gmail.com>
2015-03-20 22:33:20 -0500

    completed assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## functions allow user to cache the inverse of a square matrix
 
-## Write a short comment describing this function
+## functions based on Roger Peng's Caching the Mean of a Vector
+
+## function "makeCacheMatrix" allows user to set & get  the values of square matrix
+## and set & get the value of the inverse of the initially set square matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) inv <<- solve
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## function "cacheSolve" checks if the inverse of the square matrix set in "makeCacheMatrix"
+## is already calculated. If it is, it get the inverse, else it calculates the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
